f8e9d2da58ee3c1534b163371c2160a048a979b0
LuanSilva669 <125572605+LuanSilva669@users.noreply.github.com>
2023-05-25 22:03:51 -0300

    readme

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,7 +7,7 @@ export default class Porfolio extends Component {
       <section id="portfolio">
       <div className="row">
         <div className="twelve columns collapsed">
-          <h1>Check Out Some of My Works.  </h1>
+          <h1>Check Out Some of My Works.        <a href='https://github.com/stars/LuanSilva669/lists/my-stack' target='blank'>   Project repository</a>  </h1>
           <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
           
             {
